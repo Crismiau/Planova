042ca9e386f4fc1b1421d76b713cd21e05648ccd
Crismiau <heycristian26@gmail.com>
2025-11-13 07:05:55 -0500

    struct: Add configurations and structurs of Intellij Idea

---
+++ PlanovaCode/src/main/java/com/Planova/PlanovaCode/PlanovaCodeApplication.java
@@ -0,0 +1,13 @@
+package com.Planova.PlanovaCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanovaCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanovaCodeApplication.class, args);
+	}
+
+}
---
+++ PlanovaCode/src/test/java/com/Planova/PlanovaCode/PlanovaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Planova.PlanovaCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanovaCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
